f2406f2032a96d608770ddf3d8754859242760be
yunchungchang <69692193+yunchungchang@users.noreply.github.com>
2020-08-14 11:02:22 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}   System.out.println("My name is Yun Chung Chang");
+	    System.out.println("I like to play basketball");
+	    System.out.println("I need my bio credit");
+	    System.out.println("I own two puppies");
+	    System.out.println("APCS ROCK");
+	
+}
