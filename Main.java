3f263784670269bcb07e67cdd23aae2dc1b376ac
yunchungchang <69692193+yunchungchang@users.noreply.github.com>
2020-08-18 20:52:44 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,25 @@
-/******************************************************************************
+import java.util.Scanner;
 
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
 
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}   System.out.println("My name is Yun Chung Chang");
-	    System.out.println("I like to play basketball");
-	    System.out.println("I need my bio credit");
-	    System.out.println("I own two puppies");
-	    System.out.println("APCS ROCK");
-	
+	    final double PI = 3.1415926;
+	    Scanner reader = new Scanner(System.in);
+	    System.out.println("Enter the distance to target.\n");
+	    double d = reader.nextDouble();
+	    System.out.println("Enter the height of the battery relative ti the water line.\n");
+	    double h = reader.nextDouble();
+	    System.out.println("Enter the initial velocity of the shell.\n");
+	    double v = reader.nextDouble();
+	    
+	    double a = -4.9*(d/v)*(d/v);
+	    double b = d;
+	    double c = a + h;
+	    
+	    double tanTheta = (-b + Math.sqrt(b*b - 4*a*c))/(2*a);
+	    double angle = Math.atan(tanTheta)*180/PI;
+	    System.out.println("The angle is" + angle);
+	    
+	}
 }
